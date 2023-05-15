68bc26e2eec54553e1d53100e0a37434dd4530fa
Raji Johnson <rjohnson1906@gmail.com>
2023-05-15 18:43:20 -0400

    Updated Moving Into: Deep Dive: Props, State, and this.(Part1)

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import moment from "moment";
 
+import PortfolioContainer from './portfolio/portfolio-container';
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Raji Johnson React Portfolio</h1>
-        <h2>React Redux Router</h2>
         <div>
           {moment().format('MMMM Do YYYY, h:mm:ss a')}
         </div>
+        <h2>React Redux Router</h2>
+        <PortfolioContainer />
       </div>
     );
   }
---
+++ src/components/portfolio/portfolio-container.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+
+import PortfolioItem from "./portfolio-item";
+
+export default class PortfolioContainer extends Component {
+    constructor() {
+      super();
+
+
+      this.state = {
+        pageTitle: "Welcome to My Portfolio",
+        data: [
+            {title: "Quip" }, 
+            {title:"Eventbrite" }, 
+            {title:"Ministry Safe" }, 
+            {title:"Swingaway" }
+        ]
+      };
+
+    }
+
+    portfolioItems() {
+
+        return this.state.data.map(item => {
+            return <PortfolioItem title={item.title} url={"google.com"} />;
+        })
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h2>{this.state.pageTitle}</h2>
+
+                {this.portfolioItems()}
+
+            </div>
+        )
+    }
+}
