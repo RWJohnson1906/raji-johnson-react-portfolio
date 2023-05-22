cfd458cfccb4823887bbd91ea863a772bbcc3ebe
Raji Johnson <rjohnson1906@gmail.com>
2023-05-22 15:57:07 -0400

    Update Following Route Setup

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -8,15 +8,27 @@ export default class PortfolioContainer extends Component {
 
     this.state = {
       pageTitle: "Welcome to my portfolio",
+      isLoading: false,
       data: [
-        { title: "Quip" },
-        { title: "Eventbrite" },
-        { title: "Ministry Safe" },
-        { title: "SwingAway" }
+        { title: "Quip", category: "eCommerce" },
+        { title: "Eventbrite", category: "Scheduling" },
+        { title: "Ministry Safe", category: "Enterprise" },
+        { title: "SwingAway", category: "eCommerce" }
       ]
     };
 
-    this.handlePageTitleUpdate = this.handlePageTitleUpdate.bind(this);
+    this.handleFilter = this.handleFilter.bind(this);
+
+  }
+
+
+  handleFilter(filter) {
+    this.setState({
+      data: this.state.data.filter(item => {
+        return item.category === filter;
+      }) 
+    })
+
   }
 
   portfolioItems() {
@@ -25,22 +37,20 @@ export default class PortfolioContainer extends Component {
     });
   }
 
-  handlePageTitleUpdate() {
-    this.setState({
-      pageTitle: "Something Else"
-    });
-  }
-
   render() {
+    if (this.state.isLoading) {
+      return <div>Loading...</div>;
+    }
     return (
       <div>
         <h2>{this.state.pageTitle}</h2>
 
+        <button onClick={() => this.handleFilter('eCommerce')}>eCommerce</button>
+        <button onClick={() => this.handleFilter('Scheduling')}>Scheduling</button>
+        <button onClick={() => this.handleFilter('Enterprise')}>Enterprise</button>
+
         {this.portfolioItems()}
 
-        <hr />
-
-        <button onClick={this.handlePageTitleUpdate}>Change Title</button>
       </div>
     );
   }
